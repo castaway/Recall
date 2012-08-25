71ea3d69efa36e4e3aeee54c328e44211a662973
David Dorward <david@dorward.me.uk>
2012-08-25 10:15:38 +0100

    Fetch all content from old CMS and insert it into the new system

--- import/sBuilder/import_from_sBuilder.pl
+++ import/sBuilder/import_from_sBuilder.pl
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 
 use DateTime;
+use File::Slurp;
 
 use lib qw[ 
 	../../lib/ 
@@ -13,31 +14,58 @@ use lib qw[
 use Recall::Schema::DB;
 use Recall::Slug;
 use SBuilder::DataSource::Resources;
+use SBuilder::DataSource::MetaData;
+
+# Great password, eh? Good luck connecting to the server running this and not containing any data I care about...
+my $recall = Recall::Schema::DB->connect('dbi:mysql:recall', 'recall', 'recall'); 
+my $documents = $recall->resultset('Document');
 
 my @blog_entries  = SBuilder::DataSource::Resources->search({ action => 'Blog' });
-
-my @uniqueness;
 
 foreach my $entry (@blog_entries) {
 	
 	my $id = $entry->id;
     my $path = $entry->path;
 
+    my $date_slug;
+
     # Special cases for broken data. TODO: Make sure the redirects are implemented to support this!
     if ($path eq '/2006/12/xml-rai.html') {
-    	$path = "/2006/12/18/xml-rai.html";
+    	$date_slug = "/2006/12/18/xml-rai.html";
+    } elsif ($path eq '/2006/12/css-validator.html') {
+    	$date_slug = "/2006/12/18/css-validator.html";
+    } else {
+    	$date_slug = $path;
     }
-	if ($path eq '/2006/12/css-validator.html') {
-    	$path = "/2006/12/18/css-validator.html";
-    }    
 
-    my ($year, $month, $day, $slug) = ($path =~ m!/(\d+)/(\d+)/(\d+)/(.+)!);
+    my ($year, $month, $day, $slug) = ($date_slug =~ m!/(\d+)/(\d+)/(\d+)/(.+)!);
     
     # No constraints on what a slug can be for legacy content
     # $slug = Recall::Slug->new( name => $slug )->slug;
 
 	my $datetime = DateTime->new(year => $year, month => $month, day => $day);    
 
+
+	my $meta = SBuilder::DataSource::MetaData->find($id);;
+	my $title = $meta->title;
+	my $summary = $meta->description; # TODO add support for summaries to Recall. Ditto keywords / tags.
+
+
+    my $file = "/home/david/sBuilder/source/blog.dorward.me.uk" . $path;
+    my $content = read_file($file);
+
+    # TODO : Parse out the image URIs, fetch the images, upload them a service, then update the content
+
     say "$id - $slug - $path - $datetime";
+
+    my $doc = $documents->create({});
+    my $ver = $doc->create_related('versions', { 
+			edited => $datetime, 
+			title => $title, 
+			source => $content 
+		});
+    $doc->first_published($ver);
+    $doc->live($ver);
+    $doc->update;
 
 }
