2d8f3843ae859b3d27719a49dccca2be8f62c594
David Dorward <david@dorward.me.uk>
2013-01-08 18:26:39 +0000

    Have an Apache config that REALLY works
    This time tested on static and dynamic content instead of just static content. (le sigh).

--- script/recall_fastcgi.pl
+++ script/recall_fastcgi.pl
@@ -1,23 +1,46 @@
 #!/usr/bin/env perl
 
 # # # Working dev Apache configuration
+# Via http://wiki.catalystframework.org/wiki/gettingstarted/tutorialsandhowtos/layering_static_content_over_your_catalyst_applcation_with_apache_and_fastcgi
 # <VirtualHost *:80>
 #         DocumentRoot  /home/david/prog/recall/http_root
 #         ServerName recall.local
 #         ErrorLog /home/david/prog/recall/logs/error.log
 #         CustomLog /home/david/prog/recall/logs/access.log combined
 
-#         SetHandler fcgid-script
-#         Options +ExecCGI
-#         SetEnv PERL5LIB /home/david/perl5/lib/perl5/i486-linux-gnu-thread-multi:/home/david/perl5/lib/perl5:/home/david/prog/recall/Recall/lib
-#         #Alias /static/ "home/david/prog/recall/Recall/static/"
-#         AliasMatch ^/(^static|^favicon/).*$ /home/david/prog/recall/Recall/script/recall_fastcgi.pl/
-
 #         <Location /static/>
 #                 SetHandler default-handler
 #         </Location>
 
+#         Alias /static/ /home/david/prog/recall/Recall/root/static/
+#         Alias / /home/david/prog/recall/Recall/script/recall_fastcgi.pl/
+
+#         SetHandler fcgid-script
+#         Options +ExecCGI
+#         SetEnv PERL5LIB /home/david/perl5/lib/perl5/i486-linux-gnu-thread-multi:/home/david/perl5/lib/perl5:/home/david/prog/recall/Recall/lib 
+
+#         # Make sure the rewriting phase skips http://example.net/static
+#         # and http://example.net/.fcgi
+#         RewriteCond $1 !^(static|.fcgi)/?$
+
+#         # If a file OR directory matching the request exists...
+#         RewriteCond "/var/www/static/$1" -f [OR]
+#         RewriteCond "/var/www/static/$1/index.html" -f
+
+#         # ...then rewrite the request to use our fake static
+#         # content alias (which is actually the DocumentRoot)
+#         RewriteRule ^/(.*)$ /static/$1
+
+#         # If the request is for a directory, then make sure to serve up index.html
+#         RewriteCond "/var/www/static/$1" -d
+#         RewriteRule ^/static/(.*)/?$ /static/$1/index.html
+
+#         # Finally, signal that rewriting is done and pass-through
+#         # the request to the alias handler
+#         RewriteRule ^/static/ - [L,PT]
+
 # </VirtualHost>
+
 
 use lib qw[
     /home/david/perl5/lib/perl5/i486-linux-gnu-thread-multi
