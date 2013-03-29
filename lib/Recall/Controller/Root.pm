8e6a0d30d4b57e5b61d786f775c65c5e1bba8e6d
David Dorward <david@dorward.me.uk>
2013-03-29 09:37:33 +0000

    Add a homepage and a bit of design
    This is some rather old code that I failed to check in last time I was looking at this app.

--- lib/Recall/Controller/Root.pm
+++ lib/Recall/Controller/Root.pm
@@ -27,11 +27,9 @@ The root page (/)
 
 =cut
 
-sub index :Path :Args(0) {
+sub home :Path :Args(0) {
     my ( $self, $c ) = @_;
 
-    # Hello World
-    $c->response->body( $c->welcome_message );
 }
 
 =head2 default
